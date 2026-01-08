7396ca3774d62c4a5e0dbfb84f4b1b658a30699a
alakmar344 <proman007power@gmail.com>
2026-01-08 18:50:53 +0530

    Create utilis.js

---
+++ utilis.js
@@ -0,0 +1,10 @@
+import crypto from "crypto";
+
+export function hash(text) {
+  return crypto.createHash("sha256").update(text).digest("hex");
+}
+
+export function generateKey(plan) {
+  const r = () => Math.random().toString(36).substring(2, 6).toUpperCase();
+  return `ESAMZ-${plan.toUpperCase()}-${r()}-${r()}-${r()}`;
+}
