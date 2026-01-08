d3715033b2debb492f717868ead9ac89af948adb
alakmar344 <proman007power@gmail.com>
2026-01-08 20:13:17 +0530

    Update utilis.js

--- utilis.js
+++ utilis.js
@@ -4,7 +4,7 @@ export function hash(text) {
   return crypto.createHash("sha256").update(text).digest("hex");
 }
 
-export function generateKey(plan) {
+export function generateKey() {
   const r = () => Math.random().toString(36).substring(2, 6).toUpperCase();
-  return `ESAMZ-${plan.toUpperCase()}-${r()}-${r()}-${r()}`;
+  return `ESAMZ-ADFREE-${r()}-${r()}`;
 }
